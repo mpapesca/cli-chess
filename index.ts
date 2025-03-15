be171fcf5ec65cfe00699289e44c8799cd5a05f2
Michael Papesca <michael.papesca@gmail.com>
2025-03-15 00:03:33 -0600

    Add coloring to movement

--- index.ts
+++ index.ts
@@ -19,14 +19,23 @@ const getColor = (player: 1 | 2) => {
   }
 }
 
-const wrapPieceIcon = (icon?: string, withPointer = false) => {
-  const wrapper = withPointer ? chalk.underline : (a: unknown) => a;
-  return ` ${icon ? wrapper(icon) : ' '} `;
+const wrapPieceIcon = (icon?: string, withPointer = false, isSelected = false) => {
+  let formattedIcon = icon !== ' ' ? icon : withPointer ? "X" : " ";
+
+  if (withPointer && isSelected) {
+    formattedIcon = chalk.cyan(formattedIcon);
+  } else if (withPointer) {
+    formattedIcon = chalk.white(formattedIcon);
+  } else if (isSelected) {
+    formattedIcon = chalk.blue(formattedIcon);
+  }
+
+  return ` ${formattedIcon} `;
 };
 
 
 
-const renderBoard = (board: Board, pointerPosition?: [number, number]) => {
+const renderBoard = (board: Board, pointerPosition?: [number, number], selectedPiece?: Piece | null) => {
   const capturedPieces: { [key in 1 | 2]: Piece[]} = {
     1: [],
     2: [],
@@ -40,12 +49,13 @@ const renderBoard = (board: Board, pointerPosition?: [number, number]) => {
       if (!piece) {
         return wrapPieceIcon(emptyPosition, isPointer);
       }
+      const isSelected = selectedPiece?.position === piece.position;
       if (!piece.position) {
         capturedPieces[piece.player === 1 ? 2 : 1].push(piece as Piece);
         return wrapPieceIcon(emptyPosition, isPointer);
       }
       const color = getColor(piece.player);
-      return wrapPieceIcon(`${color(icons[piece.type])}`, isPointer);
+      return color(wrapPieceIcon(`${icons[piece.type]}`, isPointer, isSelected));
     }).join("|")
     return `${8 - (rowIndex)} |${rowString}|`;
   }).join(border);
@@ -64,9 +74,16 @@ const renderBoard = (board: Board, pointerPosition?: [number, number]) => {
   console.log(renderedBoard);
 }
 
+const getPiece = (board: Board, pointer: [number, number]) => {
+  const [row, column] = pointer;
+  const piece = board[7 - row][column];
+  return piece;
+}
 const run = async () => {
   let pointer: [number, number] = [0, 0];
   let lastPointer: [number, number] = [0, 0];
+  let selectedPiece: Piece | null = null;
+  let shouldUpdateBoard = false;
   
   // move cursor with arrow keys
   rl.input.on("keypress", (_string: string, key: { name: string, ctrl: boolean }) => {
@@ -75,13 +92,43 @@ const run = async () => {
     }
     if (key.name === "up") {
       pointer[0] = Math.min(pointer[0] + 1, 7);
+      shouldUpdateBoard = true;
     } else if (key.name === "down") {
       pointer[0] = Math.max(pointer[0] - 1, 0);
+      shouldUpdateBoard = true;
     } else if (key.name === "left") {
       pointer[1] = Math.max(pointer[1] - 1, 0);
+      shouldUpdateBoard = true;
     } else if (key.name === "right") {
       pointer[1] = Math.min(pointer[1] + 1, 7);
+      shouldUpdateBoard = true;
+    } else if (key.name === "m") {
+      if (selectedPiece) {
+        // //move piece
+        // const [row, column] = pointer;
+        // const newBoard = [...startingBoard];
+        // newBoard[7-row][column] = selectedPiece;
+        // newBoard[7-selectedPiece.position[0]][selectedPiece.position[1]] = null;
+        // startingBoard = newBoard;
+        // selectedPiece = null;
+      } else {
+        //select piece
+        const piece = getPiece(startingBoard, pointer);
+        if (piece) {
+          selectedPiece = piece;
+          // console.log(`selected piece: ${ piece.type } at ${ piece.position }`);
+          shouldUpdateBoard = true;
+        }
+      }
     }
+    else if (key.name === "escape") {
+      if (selectedPiece) {
+        // deselect piece
+        selectedPiece = null;
+        shouldUpdateBoard = true;
+      }
+    } 
+
   });
 
   const resetPosition = () => {
@@ -94,11 +141,13 @@ const run = async () => {
   renderBoard(startingBoard, pointer);
   while (true) {
     
-    if (lastPointer[0] !== pointer[0] || lastPointer[1] !== pointer[1]) {
+    if (shouldUpdateBoard) {
+      shouldUpdateBoard = false;
       lastPointer[0] = pointer[0];
       lastPointer[1] = pointer[1];
       resetPosition();
-      renderBoard(startingBoard, pointer);
+      renderBoard(startingBoard, pointer, selectedPiece);
+      readline.clearLine(process.stdout, 0)
     }
     await new Promise((resolve) => setTimeout(() => {
       // console.log('checking for move');
